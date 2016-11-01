078a49ac923b14b291de7de7cd893503a59a300b
Jacob Geryk <jacobgeryk@gmail.com>
2016-10-31 21:11:25 -0400

    adding api key and starting code for simple classify

--- musixmatch.py
+++ musixmatch.py
@@ -1,2 +1,4 @@
 import urllib2
 content = urllib2.urlopen("url_here").read()
+
+API_KEY1 = cc6a81644d36b107b7066a6c78383bda
--- simple-averaging.py
+++ simple-averaging.py
@@ -2,3 +2,8 @@ import pandas as pd
 data = pd.read_csv('Ratings_Warriner_et_al.csv')
 
 # Index a word like this data[data['Word'] == 'aardvark']
+def classify(lyrics):
+    avg_valence = 0
+    avg_arousal = 0
+    for word in lyrics:
+    pass
