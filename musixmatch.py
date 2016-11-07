a04df484aeec3381cff98d304794f193a140cd77
Jacob Geryk <jacobgeryk@gmail.com>
2016-11-06 23:41:14 -0500

    Additional API functions

--- musixmatch.py
+++ musixmatch.py
@@ -2,6 +2,7 @@ import urllib2
 import json
 
 # Music genre list for an artist: qr['message']['body']['artist_list'][0]['artist']['primary_genres']['music_genre_list']
+# 23731396
 
 class MusixMatch:
     def __init__(self, key):
@@ -23,11 +24,23 @@ class MusixMatch:
             album_ids.push(album['album']['album_id'])
         return album_list
 
-    def get_album_tracks(self, album):
-        pass
+    def get_album_tracks(self, albumid):
+        track_ids = []
+        query_string =  self.ROOT_URL + 'album.tracks.get?album_id=' + albumid '&page=1&page_size=2' + self.API_KEY
+        qr = json.loads(urllib2.urlopen(query_string).read())
+        track_list = qr['message']['body']['track_list']
+        for track in track_list:
+            track_ids.push(track['track']['track_id'])
+        return track_ids
 
     def get_lyrics(self, trackid):
+        lyrics = {}
         pass
 
     def get_lyric_discography(artist):
+        albums = self.get_albums(artist)
+        for album_id in albums:
+            tracks = get_album_tracks(album_id)
+            for track in tracks:
+                pass
         pass
