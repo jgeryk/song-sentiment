e8ad8777df97d188c2d0ef9483f5356bcd5f60b7
Jacob Geryk <jacobgeryk@gmail.com>
2016-11-13 17:00:24 -0500

    adding nb

--- process.py
+++ process.py
@@ -2,12 +2,14 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from collections import defaultdict
 import re
 import os
+import simple-averaging
+import naivebayes
 
 lmtzr = WordNetLemmatizer()
 NEG_TAG = '-'
 POS_TAG = '+'
 NEU_TAG = '0'
-PATH_TO_TRAIN = os.getcwd() + '/lyrics_annotated/test/'
+PATH_TO_TRAIN = os.getcwd() + '/lyrics_annotated/train/'
 
 def read_lyrics_from_file(file):
     words = []
@@ -15,6 +17,22 @@ def read_lyrics_from_file(file):
     for line in f:
         words.append(line)
     return words
+
+def tokenize_doc(doc):
+    """
+
+    Tokenize a document and return its bag-of-words representation.
+    doc - a string representing a document.
+    returns a dictionary mapping each word to the number of times it appears in doc.
+    """
+    bow = defaultdict(float)
+    # lowered_tokens = map(lambda t: t.lower(), tokens)
+    for lines in doc:
+        tokens = lines.split()
+        lowered_tokens = map(lambda t: t.lower(), tokens)
+        for token in lowered_tokens:
+            bow[token] += 1.0
+    return bow
 
 def get_lemma(word):
     lemma_n = lmtzr.lemmatize(word)
@@ -27,55 +45,23 @@ def get_lemma(word):
         return lemma_n
     return lemma_v
 
-class NaiveBayes:
-    def __init__(self):
-        # Vocabulary is a set that stores every word seen in the training data
-        self.vocab = set()
-        # class_total_doc_counts is a dictionary that maps a class (i.e., pos/neg) to
-        # the number of documents in the training set of that class
-        self.class_total_doc_counts = { POS_TAG: 0.0,
-                                        NEG_TAG: 0.0,
-                                        NEU_TAG: 0.0 }
-
-        # class_total_word_counts is a dictionary that maps a class (i.e., pos/neg) to
-        # the number of words in the training set in documents of that class
-        self.class_total_word_counts = { POS_TAG: 0.0,
-                                         NEG_TAG: 0.0,
-                                         NEU_TAG: 0.0 }
-
-        # class_word_counts is a dictionary of dictionaries. It maps a class (i.e.,
-        # pos/neg) to a dictionary of word counts. For example:
-        #    self.class_word_counts[POS_LABEL]['awesome']
-        # stores the number of times the word 'awesome' appears in documents
-        # of the positive class in the training documents.
-        self.class_word_counts = { POS_TAG: defaultdict(float),
-                                   NEG_TAG: defaultdict(float),
-                                   NEU_TAG: defaultdict(float) }
-    def top_n(self, label, n):
-        """
-
-        Returns the most frequent n tokens for documents with class 'label'.
-        """
-        return sorted(self.class_word_counts[label].items(), key=lambda (w,c): -c)[:n]
-        # def update_bow(lemmas, class):
-
-
 nb = NaiveBayes()
 filenames = os.listdir(PATH_TO_TRAIN)
 for f in filenames:
-    vocab = set()
+    print f
     lyrics = read_lyrics_from_file(os.path.join(PATH_TO_TRAIN,f))
     classification = lyrics.pop(0).rstrip().split(',')
     sentiment = classification.pop(0)
-    nb.class_total_doc_counts[sentiment] += 1
-    for line in lyrics:
-        words = line.split()
-        for word in words:
-            nb.class_total_word_counts[sentiment] += 1
-            nb.class_word_counts[sentiment][word] += 1
-            word = re.sub("[^a-zA-Z]+", "", word)
-            word = word.lower()
-            lemma = get_lemma(word)
-            vocab.add(lemma)
+    words = tokenize_doc(lyrics)
+    nb.update_model(words, sentiment)
+nb.report_statistics_after_training()
+    # for word in words:
+    #     nb.class_total_word_counts[sentiment] += 1
+    #     word = re.sub("[^a-zA-Z]+", "", word)
+    #     word = word.lower()
+    #     lemma = get_lemma(word)
+    #     nb.class_word_counts[sentiment][lemma] += 1
+    #     vocab.add(lemma)
+
 # print nb.class_total_doc_counts
 # print nb.top_n('-', 10)
