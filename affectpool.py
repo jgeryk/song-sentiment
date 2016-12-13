03802b9f0b828e68924edc5b9eb1ecfe1103fe15
Jacob Geryk <jacobgeryk@gmail.com>
2016-12-13 12:24:18 -0500

    fixing bug for affect classification in nb

--- affectpool.py
+++ affectpool.py
@@ -12,18 +12,20 @@ class AffectPool:
     def simple_train(self, training_set):
         self.nb.train_model(training_set)
         # avgs = self.sa.train(training_set)
-        for lemma in ['hate', 'happy']:
+        for lemma in ['pain', 'happy', 'sunshine', 'sleep']:
             lemma_affects = self.sa.sentiment_data[self.sa.sentiment_data['Word'] == lemma]
             if not lemma_affects.empty:
                 self.lemma_data[lemma]['Valence'] = 2.5*(float(lemma_affects['V.Mean.Sum']) - 5)
                 self.lemma_data[lemma]['Arousal'] = 2.5*(float(lemma_affects['A.Mean.Sum']) - 5)
-            sentiment_probs = self.nb.lemma_max_likelihoods_simple(lemma, 0.3)
-            affect_probs = self.nb.lemma_max_likelihoods_affect(lemma, 0.3)
+            sentiment_probs = self.nb.lemma_max_likelihoods_simple(lemma, 0.3, len(training_set))
+            affect_probs = self.nb.lemma_max_likelihoods_affect(lemma, 0.3, len(training_set))
             # most_likely_ = max(stats, key=stats.get)
             # affect_probs =
-            print sentiment_probs
-            print affect_probs
-            print 'ay'
+            print lemma
+            # print sentiment_probs
+            print sentiment_probs[max(sentiment_probs.keys())]
+            # print affect_probs
+            print affect_probs[max(affect_probs.keys())]
 
 
     def evaluate():
